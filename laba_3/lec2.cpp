655dcbe4e80d5507496dc3eaa39cf9e73bff356d
ep113perchun <vlad.perchun.93@gmail.com>
2022-09-30 06:28:24 +0300

    perenos_na_mac

---
+++ laba_3/lec2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class clas
+{
+private:
+    
+public:
+    clas
+    ();
+    ~clas
+    ();
+};
+
+clas::clas()
+{
+}
+
+clas::~clas()
+{
+}
+
+int main(){
+
+    return 0;
+}
